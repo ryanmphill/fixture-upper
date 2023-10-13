7dd2d82455ba72ef75e54c29945ee2a79de4a246
Ryan Phillips <ryanmphill@gmail.com>
2023-10-13 12:42:27 -0500

    Formatted JSX and adjusted styling

--- src/App.jsx
+++ src/App.jsx
@@ -69,56 +69,56 @@ function App() {
 
   return (
     <>
-      <header>
-        <h1>Fixture Upper</h1>
-        <h3>Convert your raw JSON data to Django fixtures in seconds!</h3>
+      <header className="fixture_upper__header">
+        <h1 id="title">Fixture Upper</h1>
+        <h3 id="subtitle">Convert your JSON data to Django fixtures in seconds!</h3>
       </header>
-      <section className="fixture_upper__formSection">
-        <div className="fixture_upper__formField">
-          <label htmlFor="userModelInput">Name of Model</label>
-          <input 
-          className="fixture_upper--input"
-          id="userModelInput"
-          placeholder="Name of Model"
-          ref={userModelInput}
-          />
-          {errorMessages.includes("modelError") && 
-          <div style={{color: 'red'}}>
-            Please input the name of your Django model
-          </div>}
-        </div>
-        <div className="fixture_upper__formField">
-          <label htmlFor="userJSONInput">Paste Your JSON Data Here</label>
-          <textarea
-          className="fixture_upper--input"
-          id="userJSONInput"
-          placeholder={exampleData}
-          ref={userJSONinput}
-          >
-          </textarea>
-        </div>
-        
-        {errorMessages.includes("JSONError") && 
-        <div style={{color: 'red'}}>
-          Please make sure your input is valid JSON ex: &#123; "property": "value" &#125;
-        </div>}
-        
-        <button onClick={(e) => handleSubmit(e)}>Submit</button>
-      </section>
-      <section className="fixture_upper__codeSection">
-        {
-          JSONData.length > 0 && <>
-            <CopyToClipboard text={JSONData}
-              onCopy={() => setCopyState({ ...copyState, copied: true })}>
-              <button>Copy to clipboard 📋</button>
-            </CopyToClipboard>
-            {copyState?.copied ? <div style={{ color: 'red' }}>Copied.</div> : <></>}
-            <SyntaxHighlighter language="javascript" style={darcula} id="codeBlock">
-              {JSONData}
-            </SyntaxHighlighter>
-          </>
-        }
-      </section>
+      <div className="fixture_upper__container">
+        <section className="fixture_upper__formSection">
+          <div className="fixture_upper__formField">
+            <label htmlFor="userModelInput">Name of Model</label>
+            <input
+              className="fixture_upper--input"
+              id="userModelInput"
+              placeholder="Name of Model"
+              ref={userModelInput}
+            />
+            {errorMessages.includes("modelError") &&
+              <div className="error_message">
+                Please input the name of your Django model
+              </div>}
+          </div>
+          <div className="fixture_upper__formField">
+            <label htmlFor="userJSONInput">Paste Your JSON Data Here</label>
+            <textarea
+              className="fixture_upper--input"
+              id="userJSONInput"
+              placeholder={exampleData}
+              ref={userJSONinput}
+            >
+            </textarea>
+          </div>
+
+          {errorMessages.includes("JSONError") &&
+            <div className="error_message">
+              Please make sure your input is an array of valid JSON data ex: &#91; &#123; "property": "value" &#125; &#93;
+            </div>}
+
+          <button onClick={(e) => handleSubmit(e)}>Submit</button>
+        </section>
+      </div>
+      {
+        JSONData.length > 0 && <section className="fixture_upper__codeSection">
+          <CopyToClipboard text={JSONData}
+            onCopy={() => setCopyState({ ...copyState, copied: true })}>
+            <button>Copy to clipboard 📋</button>
+          </CopyToClipboard>
+          {copyState?.copied ? <div style={{ color: 'red' }}>Copied.</div> : <></>}
+          <SyntaxHighlighter language="javascript" style={darcula} id="codeBlock">
+            {JSONData}
+          </SyntaxHighlighter>
+        </section>
+      }
     </>
   )
 }
